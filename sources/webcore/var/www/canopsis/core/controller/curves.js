9d6fb0d0e8b63c1768542351ba1d9e0ee0a8750d
David Delassus <david.jose.delassus@gmail.com>
2014-01-20 17:58:42 +0100

    add: Curves view

---
+++ sources/webcore/var/www/canopsis/core/controller/curves.js
@@ -0,0 +1,80 @@
+define(function(require, exports, module) {
+	Ember.TEMPLATES['curves'] = Ember.Handlebars.compile(require('text!app/templates/curves.html'));
+
+	var Application = require('app/application');
+
+	Application.CurvesRoute = Ember.Route.extend({
+		model: function() {
+			var me = this;
+
+			return $.ajax({
+				url: '/rest/object/curve',
+				method: 'GET',
+				contentType: 'application/json'
+			}).then(function(data, status, xhr) {
+				var curves = [];
+
+				for(var i = 0; i < data.data.length; i++) {
+					var curve = data.data[i];
+
+					curves.push({
+						line_color: '#' + curve.line_color,
+						area_color: '#' + curve.area_color,
+						line_style: curve.dashStyle,
+						area_opacity: curve.area_opacity,
+						zindex: curve.zIndex,
+						invert: curve.invert,
+						metric: curve.metric,
+						label: curve.label
+					});
+				}
+
+				/* return final model */
+				var controller = me.controllerFor('curves');
+
+				return {
+					'toolitems': controller.toolbar,
+					'curves': curves
+				};
+			});
+		}
+	});
+
+	Application.CurvesController = Ember.ObjectController.extend({
+		toolbar: [{
+			title: 'Refresh',
+			action: 'refresh',
+			icon: 'refresh'
+		},{
+			title: 'Add',
+			action: 'add',
+			icon: 'plus-sign'
+		},{
+			title: 'Duplicate',
+			action: 'duplicate',
+			icon: 'file'
+		},{
+			title: 'Remove',
+			action: 'remove',
+			icon: 'trash'
+		}],
+
+		actions: {
+			do: function(action) {
+				this.send(action);
+			},
+
+			refresh: function() {
+				;
+			},
+
+			duplicate: function() {
+				;
+			},
+
+			remove: function() {
+				;
+			}
+		}
+	});
+});
