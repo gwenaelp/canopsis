20dd1f30f69c274d5af34248dca3b4d7b441ec2c
mrbozzo <jlabejof@yahoo.fr>
2014-02-23 10:23:34 +0100

    change the couple 'manual_reconfiguration/reconfigure' with one field reconfiguration which can takes values {AUTO=1; MANUAL=0; ONCE=3}

--- sources/canolibs/lib/canolibs/cconfiguration.py
+++ sources/canolibs/lib/canolibs/cconfiguration.py
@@ -27,185 +27,136 @@ CONFIGURATION_DIRECTORY = os.path.expanduser('~/etc/')
 # GLOBAL CONFIGURATION FILE
 CONFIGURATION_FILE = 'configuration.conf'
 
-import pyinotify
-
 # KEYS FOR CONFIGURATION FILE
 # DEFAULT SECTION
 DEFAULT = 'DEFAULT'  # GLOBAL SECTION
-MANUAL_RECONFIGURATION = 'manual_reconfiguration'  # KEY FOR DEFINING MANUAL_RECONFIGURATION.
+RECONFIGURATION = 'reconfiguration'  # KEY FOR MANUAL/AUTO/ONCE RECONFIGURATION.
+MANUAL = 0  # manual reconfiguration
+AUTO = 1  # auto reconfiguration
+ONCE = 2  # do a reconfiguration
+ONCE_MANUAL = ONCE | MANUAL  # ONCE then MANUAL mode
+ONCE_AUTO = ONCE | AUTO  # ONCE then AUTO mode
 LAST_RECONFIGURATION = 'last_reconfiguration'  # DATE OF LAST DEFAULT/LOCAL RECONFIGURATION.
-RECONFIGURE = 'reconfigure'  # FLAGS USED TO DO A DEFAULT/LOCAL RECONFIGURATION.
 
 # SPECIFIC TO SECTIONS
-FILE = 'file'  # CONFIGURATION FILE PATH
-LISTENER = 'listener'  # CONFIGURATION FILE LISTENER.
+PATH = 'path'  # CONFIGURATION FILE PATH
+WATCHER = 'watcher'  # CONFIGURATION FILE WATCHER.
+NOTIFIER = 'notifier'  # NOTIFIER
 
-import clogging
-_logger = clogging.getLogger()
+import logging
+logger = logging.getLogger()
+
+from pyinotify import WatchManager, ThreadedNotifier, ProcessEvent, IN_MODIFY, IN_CREATE
 
 
-class EventHandler(pyinotify.ProcessEvent):
-    """
-    File system handler which listen modification of configuration directory
-    content and notifies configuration file bound observers.
-    """
+class ConfigurationManager(object):
 
-    MASK = pyinotify.IN_MODIFY | pyinotify.IN_CREATE  # watched events
+    def __init__(
+        self,
+        path=CONFIGURATION_DIRECTORY,
+        file_name=CONFIGURATION_FILE,
+        reconfiguration=MANUAL
+    ):
+        self.path = path
+        self.file_name = file_name
+        self.reconfiguration = reconfiguration
+        self.watchers = dict()
 
-    def __init__(self):
-
-        super(EventHandler, self).__init__()
-
-        self.resources = dict()
-        self.listeners_per_file = dict()
-        self.watch_manager = pyinotify.WatchManager()
-        self.notifier = pyinotify.ThreadedNotifier(self.watch_manager, self)
+        self.in_watching = False
         self.config_parser = RawConfigParser()
 
-        # register self among observers
-        self.register_observer(
-            CONFIGURATION_FILE, self._check_configuration, True)
+        self.register_watcher(DEFAULT, self.watch, self.file_name, self.reconfiguration)
 
-    def read_resource(self, section, config_parser):
+    def get_file_path(self, file_name):
 
-        result = dict()
+        result = os.path.join(self.path, file_name)
+        return result
 
-        result[FILE] = config_parser.get(section, FILE)
-        listener = config_parser.get(section, LISTENER)
-        result[LISTENER] = EventHandler.resolve_listener(listener)
-        result[RECONFIGURE] = config_parser.has_option(section, RECONFIGURE)
-        result[MANUAL_RECONFIGURATION] = config_parser.has_option(section, MANUAL_RECONFIGURATION)
+    def unregister_watcher(self, name):
 
-    def update_resource(self, name, resource):
+        watcher = self.watchers.get(name)
+        if watcher is not None:
+            watcher.stop()
+            del self.watchers[name]
 
-        old_resource = self.resources.get(name)
+    def register_watcher(
+        self, name, watch, file_name, reconfiguration=MANUAL
+    ):
+        self.unregister_watcher(name)
+        watcher = Watcher(name, self, watch, file_name, reconfiguration)
+        self.watchers[name] = watcher
 
-        if old_resource is None:
-            old_resource = {FILE: str()}
-            self.resources[name] = old_resource
+    def watch(self, conf_path):
 
-        listeners = self.listeners_per_file.get(resource[FILE])
+        if self.in_watching:
+            return
+        self.in_watching = True
 
-        if listeners is None:
-            listeners = list()
-            self.listeners_per_file[resource[FILE]] = listeners
+        self.config_parser.read(conf_path)
 
-        # update file
-        if old_resource[FILE] != resource[FILE]:
-            old_listeners = self.listeners_per_file.get(old_resource[FILE])
-            if old_listeners is not None and len(old_listeners) == 1:
-                del self.listeners_per_file[old_resource[FILE]]
-                wd = self.watch_manager.get_wd(old_resource[FILE])
-                self.watch_manager.rm_watch(wd)
-                self.watch_manager.add_watch(resource[FILE], EventHandler.MASK)
-            self.listeners_per_file[resource[FILE]].append(resource[LISTENER])
-
-        self.resources[name] = resource
-
-    def _check_configuration(self, src_path):
-        """
-        Observer method.
-        """
-
-        _logger.debug('src_path: {0}'.format(src_path))
-
-        # close the watched file descriptor
-        self.watch_manager.close()
-
-        resources = dict()
-
-        self.config_parser.read(src_path)
+        self.reconfiguration = \
+            self.config_parser.getint(DEFAULT, RECONFIGURATION)
 
         for section in self.config_parser.sections():
+            try:
+                path = self.config_parser.get(section, PATH)
+                reconfiguration = self.config_parser.getint(
+                    section, RECONFIGURATION)
+                watcher = self.config_parser.get(section, WATCHER)
+                watch = ConfigurationManager.resolve_watcher(watcher)
+                self.register_watcher(
+                    section, watch, path, reconfiguration)
 
-            resource = dict()
-            resource[MANUAL_RECONFIGURATION] = self.config_parser.has_option(section, MANUAL_RECONFIGURATION)
-            resource[RECONFIGURE] = self.config_parser.has_option(section, RECONFIGURE)
-            listener = self.config_parser.get(section, LISTENER)
-            resource[LISTENER] = EventHandler.resolve_listener(listener)
-            resource[FILE] = self.config_parser.get(section, FILE)
+                if self.reconfiguration & ONCE or watcher.reconfiguration & ONCE:
+                    # delete ONCE flag
+                    watcher.reconfiguration &= AUTO
+                    # update reconfiguration and last_reconfiguration fields
+                    self.config_parser.set(
+                        section, RECONFIGURATION, watcher.reconfiguration)
+                    self.config_parser.set(
+                        section, LAST_RECONFIGURATION, time.ctime())
 
-            self.update_resource(section, resource)
+            except Exception as e:
+                logger.error(e)
 
-        if self.config_parser.has_section(DEFAULT):
-            self.manual_reconfiguration = self.config_parser.has_option(section, MANUAL_RECONFIGURATION)
-            self.reconfigure = self.config_parser.has_option(section, RECONFIGURE)
+        if self.reconfiguration & ONCE:
+            # delete ONCE flag
+            self.reconfiguration &= AUTO
 
-        # if a reconfiguration has to be done
-        if not self.manual_reconfiguration or self.reconfigure:
-            for name, resource in resources.iter_items():
-                if not resource[MANUAL_RECONFIGURATION] or resource[RECONFIGURE]:
-                    resource_src_path = os.path.join(CONFIGURATION_DIRECTORY, resource[FILE])
-                    self.callObserver(resource_src_path, resource[RECONFIGURE])
-                    self.config_parser.remove_option(section, RECONFIGURE)
-                    self.config_parser.set(name, LAST_RECONFIGURATION, time.ctime())
-            if self.config_parser.has_option(DEFAULT, RECONFIGURE):
-                self.config_parser.remove_option(DEFAULT, RECONFIGURE)
+            self.config_parser.set(DEFAULT, RECONFIGURATION, self.reconfiguration)
             self.config_parser.set(DEFAULT, LAST_RECONFIGURATION, time.ctime())
 
-        # save config_parser
-        with open(src_path, 'w') as fp:
-            self.config_parser.write(fp)
+        with open(conf_path, 'w+') as conf_file:
+            self.config_parser.write(conf_file)
 
-        self.resources = resources
+        self.in_watching = False
 
-    def callObservers(self, call=False):
-        """
-        Call all observers.
-        """
+    def call_watcher(self, name):
 
-        _logger.debug('')
+        watcher = self.watchers.get(name)
+        file_path = self.get_file_path(watcher.file_name)
+        if watcher is not None:
+            watcher.watch(file_path)
 
-        for configuration_file, observer in \
-                self.configurationObservers.iteritems():
-            src_path = os.path.join(
-                CONFIGURATION_DIRECTORY, configuration_file)
-            self.callObserver(src_path, call)
+    def stop(self):
+        for watcher in self.watchers.itervalues():
+            watcher.stop()
 
-    def register_observer(self, configuration_file, observer, call=False):
-        """
-        Register an observer bound to input configuration_file.
-        If call is True, the observer is called just after registering.
-        """
+    def start(self):
+        for watcher in self.watchers.itervalues():
+            watcher.start()
 
-        _logger.debug(
-            'configuration_file: {0}, observer: {1}, call: {2}'
-            .format(configuration_file, observer, call))
+    def _update(self, name):
 
-        self.configurationObservers[configuration_file] = observer
+        file_path = self.get_file_path(self.file_name)
+        self.config_parser.read(file_path)
+        self.config_parser.set_option(name, LAST_RECONFIGURATION, time.ctime())
 
-        if call:
-            src_path = os.path.join(
-                CONFIGURATION_DIRECTORY, configuration_file)
-            self.callObserver(src_path, call)
-
-    def callObserver(self, src_path, call=False):
-        """
-        Call observer bound to event src_path.
-        """
-
-        _logger.debug('src_path: {0}'.format(src_path))
-
-        src_path = os.path.expanduser(src_path)
-
-        if call or not self.manual_reconfiguration:
-            filename = os.path.basename(src_path)
-            observer = self.configurationObservers.get(filename, None)
-
-            if observer is not None:
-                observer(src_path)
-
-    def process_default(self, event):
-        """
-        Call when a configuration file is modified.
-        """
-
-        _logger.debug('event: {0}'.format(event))
-
-        self.callObserver(event.pathname)
+        with open(file_path, 'w+') as conf_file:
+            self.config_parser.write(conf_file)
 
     @staticmethod
-    def resolve_listener(namespace):
+    def resolve_watcher(namespace):
         """
         Try to get an object reference from a namespace.
         Raise an ImportError if namespace is not known from this environment.
@@ -225,80 +176,50 @@ class EventHandler(pyinotify.ProcessEvent):
 
         return result
 
-# singleton for configuration file system event handler
-_event_handler = EventHandler()
 
-_notifier = None
+class Watcher(ProcessEvent):
 
+    MASK = IN_MODIFY | IN_CREATE
 
-def schedule_observer():
-    _notifier = pyinotify.ThreadedNotifier(wm, _event_handler)
-    _notifier.start()
+    def __init__(
+        self,
+        name,
+        configuration_manager,
+        watch,
+        file_name,
+        reconfiguration=MANUAL
+    ):
+        self.name = name
+        self.configuration_manager = configuration_manager
+        self.watch = watch
+        self.file_name = file_name
+        self.reconfiguration = reconfiguration
 
-#import threading
-#t = threading.Thread(target=schedule_observer)
-#t.start()
+        self.watch_manager = WatchManager()
+        self.notifier = ThreadedNotifier(self.watch_manager, self)
 
-schedule_observer()
-time.sleep(0.05)
+        file_path = configuration_manager.get_file_path(file_name)
+        self.watch_manager.add_watch(file_path, Watcher.MASK)
 
-import atexit
+        if reconfiguration & ONCE:
+            self.watch(file_path)
 
+    def process_DEFAULT(self, event):
 
-@atexit.register
-def _stop_observer():
-    """
-    Stop listening of configuration file event handler on configuration directory.
-    """
+        if self.reconfiguration:
+            self.watch(event.path)
 
-    if _logger is not None:
-        _logger.debug('')
+            self.configuration_manager._update(self.name)
 
-    if _notifier is not None:
-        _notifier.stop()
+    def process_IN_MODIFY(self, event):
+        self.process_DEFAULT(event)
 
+    def process_IN_CREATE(self, event):
+        self.process_DEFAULT(event)
 
-def register_observer(configuration_file, observer, call=False):
-    """
-    Shortcut method which register an observer and bound it with input configuration_file.
-    If call is True (False by default), the observer is called just after being registered.
-    """
+    def start(self):
+        self.notifier.start()
 
-    _logger.debug(
-        'configuration_file: {0}, observer: {1}, call: {2}'
-        .format(configuration_file, observer, call))
-
-    _event_handler.register_observer(
-        configuration_file, observer, call)
-
-
-def manual_reconfiguration(manual_reconfiguration=None):
-    """
-    Returns configuration file handler manual reconfiguration status.
-    If input manual_reconfiguration is given, change the status.
-    """
-
-    _logger.debug(
-        'manual_reconfiguration: {0}'
-        .format(manual_reconfiguration))
-
-    if manual_reconfiguration is not None:
-        _event_handler.manual_reconfiguration = \
-            manual_reconfiguration
-
-    return _event_handler.manual_reconfiguration
-
-
-if __name__ == '__main__':
-    """
-    Script execution.
-    """
-
-    import sys
-
-    if len(sys.argv) > 1:
-        for conf_file in sys.argv[1:]:
-            src_path = os.path.join(CONFIGURATION_DIRECTORY, conf_file)
-            _event_handler.callObserver(src_path, True)
-    else:
-        _event_handler.callObservers(True)
+    def stop(self):
+        if self.notifier.is_alive():
+            self.notifier.stop()
--- sources/canolibs/lib/canolibs/unittest/cconfiguration-Myunittest.py
+++ sources/canolibs/lib/canolibs/unittest/cconfiguration-Myunittest.py
@@ -1,8 +1,8 @@
 import unittest
 import cconfiguration
-import os
-import time
+import os.path
 
+WATCHER_NAME = 'test'
 TEST_FILE = 'cconfiguration.conf'
 TEST_FILE_PATH = os.path.join(
     cconfiguration.CONFIGURATION_DIRECTORY, TEST_FILE)
@@ -11,35 +11,49 @@ TEST_FILE_PATH = os.path.join(
 class CconfigurationTest(unittest.TestCase):
 
     def setUp(self):
-        cconfiguration.manual_reconfiguration(False)
+        self.configuration_manager = cconfiguration.ConfigurationManager()
         self.has_been_updated = False
 
-    def _observes(self, src_path):
+    def tearDown(self):
+        self.configuration_manager.stop()
+
+    def _watch(self, src_path):
         self.has_been_updated = True
 
-    def testObserver(self):
+    def testWatcher(self):
 
-        cconfiguration.register_observer(TEST_FILE, self._observes, True)
+        self.assertFalse(self.has_been_updated)
+
+        self.configuration_manager.register_watcher(
+            WATCHER_NAME, self._watch, TEST_FILE)
+
+        self.assertFalse(self.has_been_updated)
+
+        self.has_been_updated = False
+
+        self.configuration_manager.register_watcher(
+            WATCHER_NAME, self._watch, TEST_FILE, cconfiguration.ONCE_AUTO)
 
         self.assertTrue(self.has_been_updated)
 
-        self.has_been_updated = False
-
-        cconfiguration.register_observer(TEST_FILE, self._observes)
-
-        self.assertFalse(self.has_been_updated)
+        self.configuration_manager.start()
 
         if os.path.exists(TEST_FILE_PATH):
             os.remove(TEST_FILE_PATH)
 
-        with open(TEST_FILE_PATH, 'w') as test_file:
+        self.has_been_updated = False
+
+        self.assertFalse(self.has_been_updated)
+
+        with open(TEST_FILE_PATH, 'w+') as test_file:
+
+            self.assertTrue(self.has_been_updated)
+
+            self.has_been_updated = False
+
             test_file.write('test')
 
-        #time.sleep(.5)
-
         self.assertTrue(self.has_been_updated)
-
-        pass
 
 if __name__ == '__main__':
     unittest.main()
