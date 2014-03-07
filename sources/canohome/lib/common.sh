dceb1f7a202fa8d436c6a7109822c53a9add3ef3
Gwenaël Pluchon <gpluchon@capensis.fr>
2014-03-07 12:13:40 +0100

    fix bash ifs in os detection

--- sources/canohome/lib/common.sh
+++ sources/canohome/lib/common.sh
@@ -25,35 +25,35 @@ function detect_os(){
 	ARCHL=`if [ -e /etc/arch-release ]; then echo 1; fi`
 	DIST_VERS=""
 	
-	if [ $OPT_DIST -ne 0 ]; then
+	if [ "$OPT_DIST" -ne 0 ]; then
 		DIST=$OPT_DIST
 	fi
-	if [ $OPT_DISTVERS -ne 0 ]; then
+	if [ "$OPT_DISTVERS" -ne 0 ]; then
 		DISTVERS=$OPT_DISTVERS
 		return
 	fi
 
-	if [ $DEBIAN -ne 0 ]; then
+	if [ "$DEBIAN" -ne 0 ]; then
 		DIST="debian"
 		DIST_VERS=`cat /etc/debian_version | cut -d '.' -f1`
 		echo " + $DIST $DIST_VERS"
-	elif [ $UBUNTU -ne 0 ]; then
+	elif [ "$UBUNTU" -ne 0 ]; then
 		DIST="ubuntu"
 		DIST_VERS=`lsb_release -r | cut -f2`
 		echo " + $DIST $DIST_VERS"
-	elif [ $REDHAT -ne 0 ]; then
+	elif [ "$REDHAT" -ne 0 ]; then
 		DIST="redhat"
 		DIST_VERS=`lsb_release -r | cut -f2 | cut -d '.' -f1`
 		echo " + $DIST $DIST_VERS"
-	elif [ $CENTOS -ne 0 ]; then
+	elif [ "$CENTOS" -ne 0 ]; then
 		DIST="centos"
 		DIST_VERS=`lsb_release -r | cut -f2 | cut -d '.' -f1`
 		echo " + $DIST $DIST_VERS"
-	elif [ $FC -ne 0 ]; then
+	elif [ "$FC" -ne 0 ]; then
 		DIST="fedora"
 		DIST_VERS=`lsb_release -r | cut -f2 | cut -d '.' -f1`
 		echo " + $DIST $DIST_VERS"
-	elif [ $ARCHL -ne 0 ]; then
+	elif [ "$ARCHL" -ne 0 ]; then
 		DIST="archlinux"
 		DIST_VERS=`pacman -Q glibc | cut -d ' ' -f2 | cut -d '-' -f1`
 		echo " + $DIST $DIST_VERS"
