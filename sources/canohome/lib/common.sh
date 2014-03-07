0d120f4fdd0bfff80dca5a330713fad23a668c67
Gwenaël Pluchon <gpluchon@capensis.fr>
2014-03-07 12:45:56 +0100

    replace value check of argument by existence check

--- sources/canohome/lib/common.sh
+++ sources/canohome/lib/common.sh
@@ -25,10 +25,10 @@ function detect_os(){
 	ARCHL=`if [ -e /etc/arch-release ]; then echo 1; fi`
 	DIST_VERS=""
 	
-	if [ "$OPT_DIST" -ne "" ]; then
+	if [ -z "$OPT_DIST"]; then
 		DIST=$OPT_DIST
 	fi
-	if [ "$OPT_DISTVERS" -ne "" ]; then
+	if [ -z "$OPT_DISTVERS" ]; then
 		DISTVERS=$OPT_DISTVERS
 		return
 	fi
